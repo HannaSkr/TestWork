3436d4e075d184974cfb0079a9fe02ec9d8acd66
Hanna <skrane4ka@gmail.com>
2022-12-25 16:12:39 +0100

    run correctly after first starting. i'm in fear

---
+++ TestTask/Program.cs
@@ -0,0 +1,76 @@
+﻿/* Задача: написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма.
+При решении не рекомендуется пользоваться коллекциями.*/
+
+
+//получение числа с консоли, для того, чтобы задать размерность массива
+int GetNumberFromConsole(string message)
+{
+bool isCorrect = false;
+int result = 0;
+
+while(!isCorrect)
+{
+Console.WriteLine(message);
+
+if(int.TryParse(Console.ReadLine(), out result))
+{
+isCorrect = true;
+}
+else
+{
+Console.WriteLine("Ввели не число. Введите число");
+}
+}
+
+return result;
+}
+
+//метод, получает массив путем ввода его элементов пользователем
+string[] InitArray(int dimension)
+{
+string[] array = new string[dimension];
+
+for (int i = 0; i < dimension; i++)
+{
+Console.WriteLine($"Введите {i}-тый элемент массива ");
+array[i] = Console.ReadLine() ?? "";
+}
+
+return array;
+}
+
+//метод, создающий новый массив из элементов, длина которых 3 и меньше символов
+string[] GetShortWordArray(string[] array)
+{
+    string[] result = new string[array.Length + 1];
+    int count = 0;
+    foreach (string value in array)
+    {
+        if (value.Length <=2)
+        {
+            result[count] = value;
+            count ++;
+        }
+    }
+    return result;
+}
+
+//метод, который будет печатать массив
+void PrintArray(string[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+}
+
+int dimension = GetNumberFromConsole("Введите количество элементов массива");
+string[] startArray = InitArray(dimension);
+Console.WriteLine();
+PrintArray(startArray);
+Console.WriteLine();
+string[] finalArray = GetShortWordArray(startArray);
+PrintArray(finalArray);
