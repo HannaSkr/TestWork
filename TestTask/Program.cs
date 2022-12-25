5ef095f794ede08d4418093eae45658d0a2fa56a
Hanna <skrane4ka@gmail.com>
2022-12-25 16:39:25 +0100

    code formation

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -7,38 +7,36 @@
 //получение числа с консоли, для того, чтобы задать размерность массива
 int GetNumberFromConsole(string message)
 {
-bool isCorrect = false;
-int result = 0;
+    bool isCorrect = false;
+    int result = 0;
 
-while(!isCorrect)
-{
-Console.WriteLine(message);
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
 
-if(int.TryParse(Console.ReadLine(), out result))
-{
-isCorrect = true;
-}
-else
-{
-Console.WriteLine("Ввели не число. Введите число");
-}
-}
-
-return result;
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Введите число");
+        }
+    }
+    return result;
 }
 
 //метод, получает массив путем ввода его элементов пользователем
 string[] InitArray(int dimension)
 {
-string[] array = new string[dimension];
+    string[] array = new string[dimension];
 
-for (int i = 0; i < dimension; i++)
-{
-Console.WriteLine($"Введите {i}-тый элемент массива ");
-array[i] = Console.ReadLine() ?? "";
-}
-
-return array;
+    for (int i = 0; i < dimension; i++)
+    {
+        Console.WriteLine($"Введите {i}-тый элемент массива ");
+        array[i] = Console.ReadLine() ?? "";
+    }
+    return array;
 }
 
 //метод, создающий новый массив из элементов, длина которых 3 и меньше символов
@@ -61,11 +59,11 @@ string[] GetShortWordArray(string[] array)
 //метод, который будет печатать массив
 void PrintArray(string[] array)
 {
-for (int i = 0; i <array.Length; i++)
-{
-Console.Write($"{array[i]} ");
-}
-Console.WriteLine();
+    for (int i = 0; i <array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
 
 int dimension = GetNumberFromConsole("Введите количество элементов массива");
