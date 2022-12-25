8fe6eeb8ff480b914552dccff97f4da3fdb22ebe
Hanna <skrane4ka@gmail.com>
2022-12-25 18:09:40 +0100

    definetly last changes

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -61,7 +61,7 @@ void PrintArray(string[] array)
 {
     for (int i = 0; i <array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
+        Console.Write($" {array[i]}"); string.Join(", ", array);
     }
     Console.WriteLine();
 }
